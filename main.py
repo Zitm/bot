85bcbe46b5182e7c4a365c00df6b0a2b64d1dd55
Zitm <zitonovdmitriy@gmail.com>
2024-10-10 15:00:06 +0400

    second

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import telebot
+from telebot import types
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv("token")
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет')
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка")
+    markup.add(item1)
+
+
+bot.infinity_polling()
